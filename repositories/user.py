896530f549a15ce7c6db275f10d53b07be84f0fa
YonyUk <yonatanjoseguerraperez@gmail.com>
2025-12-10 16:34:16 -0500

    adds 'UserRepository'

---
+++ repositories/user.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from typing import Sequence
+from models import User
+from .repository import Repository
+
+class UserRepository(Repository[User]):
+
+    def __init__(self,db: AsyncSession):
+        super().__init__(User, db)
+    
+    async def get_by_email(self,email:str) -> User | None:
+        '''
+        Docstring for get_by_email
+        
+        :param email: email of the user
+        :type email: str
+        :return: the user who owns the given email
+        :rtype: User | None
+        '''
+        query = select(User).where(User.email==email)
+        result = await self._db.execute(query)
+        return result.scalar_one_or_none()
+
+    async def _try_get_instance(self, instance: User) -> User | None:
+        db_instance = await self.get_by_id(str(instance.id))
+        if not db_instance:
+            return await self.get_by_email(str(instance.email))
+        return db_instance
