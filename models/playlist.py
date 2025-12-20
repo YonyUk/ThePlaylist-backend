68cbb4ab81ed66e03451e6e5098f1c4331039010
YonyUk <yonatanjoseguerraperez@gmail.com>
2025-12-19 21:30:40 -0500

    adds 'Playlist' entity

---
+++ models/playlist.py
@@ -0,0 +1,24 @@
+from sqlalchemy import String,BigInteger,ForeignKey
+from sqlalchemy.orm import mapped_column,Mapped,relationship
+from uuid import uuid4
+from database import BaseModel
+
+class Playlist(BaseModel):
+    '''
+    Docstring for Playlist
+
+    'Playlist' entity
+    '''
+
+    __tablename__ = 'playlists'
+    
+    id:Mapped[str] = mapped_column(String,primary_key=True,default=lambda:str(uuid4()))
+    name:Mapped[str] = mapped_column(String,nullable=False)
+    likes:Mapped[int] = mapped_column(BigInteger,default=0)
+    dislikes:Mapped[int] = mapped_column(BigInteger,default=0)
+    plays:Mapped[int] = mapped_column(BigInteger,default=0)
+    description:Mapped[str] = mapped_column(String)
+    loves:Mapped[int] = mapped_column(BigInteger,default=0)
+
+    author_id:Mapped[str] = mapped_column(String,ForeignKey('users.id',ondelete='CASCADE'),nullable=False)
+    author = relationship('User',back_populates='playlists',lazy='selectin')
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.orm import mapped_column,Mapped
+from sqlalchemy.orm import mapped_column,Mapped,relationship
 from uuid import uuid4
 from database import BaseModel
 
@@ -16,3 +16,5 @@ class User(BaseModel):
     username:Mapped[String] = mapped_column(String,unique=True,index=True,nullable=False)
     email:Mapped[String] = mapped_column(String,unique=True,index=True,nullable=False)
     hashed_password:Mapped[String] = mapped_column(String,nullable=False)
+
+    playlists = relationship('Playlist',back_populates='author',cascade='all, delete-orphan')
